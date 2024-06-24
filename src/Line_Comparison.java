e8328926199104ecc4427ead786fc540e4cd6f93
Amruta Shinde <amrutashinde2610@gmail.com>
2024-06-25 00:56:36 +0530

    Calculate Length Of Line

--- src/Line_Comparison.java
+++ src/Line_Comparison.java
@@ -1,23 +1,17 @@
 
 
-    public class Line_Comparison {
-        public double isEqual(double x1,double x2,double y1,double y2){
-            double length_line = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-            return length_line;
-        }
-        public static void main(String[] args) {
+public class Line_Comparison {
 
-            Line_Comparison uc3 = new Line_Comparison();
-            double ans1_length_line=uc3.isEqual(1, 5, 4, 9);
-            double ans2_length_line=uc3.isEqual(2, 4, 5, 6);
-
-            if(ans1_length_line == ans2_length_line){
-                System.out.println("Length of  both lines are equal");
-            }else if(ans1_length_line<ans2_length_line){
-                System.out.println("Length of  line 1 is less than line 2");
-            }
-            else{
-                System.out.println("Length of line 1 is greater than line 2");
-            }
-        }
+    public void isEqual(double x1,double x2,double y1,double y2){
+        double length_line = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+        System.out.println("Length of line is :"+length_line);
     }
+
+    public static void main(String[] args) {
+
+        Line_Comparison uc1 = new Line_Comparison();
+        uc1.isEqual(1, 5, 4, 9);
+
+
+    }
+}
