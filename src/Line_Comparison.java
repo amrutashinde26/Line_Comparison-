1f738640542bc939ac78c349815c9a04d5d0855a
Amruta Shinde <amrutashinde2610@gmail.com>
2024-06-25 01:01:23 +0530

    Check Equality Of Two Lines

--- src/Line_Comparison.java
+++ src/Line_Comparison.java
@@ -1,16 +1,24 @@
 
 
 public class Line_Comparison {
-
-    public void isEqual(double x1,double x2,double y1,double y2){
+    public double isEqual(double x1,double x2,double y1,double y2){
         double length_line = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-        System.out.println("Length of line is :"+length_line);
+        return length_line;
     }
-
     public static void main(String[] args) {
 
-        Line_Comparison uc1 = new Line_Comparison();
-        uc1.isEqual(1, 5, 4, 9);
+        Line_Comparison uc2 = new Line_Comparison();
+        double ans1_length_line=uc2.isEqual(2, 4, 5, 6);
+        double ans2_length_line=uc2.isEqual(2, 4, 5, 6);
+
+        if(ans1_length_line == ans2_length_line){
+            System.out.println("Length of  both lines are equal");
+        }
+        else{
+            System.out.println("Length of  both lines are not equal");
+
+        }
+
 
 
     }
